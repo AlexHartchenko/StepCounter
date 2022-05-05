00da8826d637161010e55a35a5ae7f9c2020bfb7
Alex Hartchenko <aj.tomsk@mail.ru>
2022-05-05 21:44:33 +0700

    Commit 2

--- app/src/main/java/com/example/simplestepscounter/MainActivity.java
+++ app/src/main/java/com/example/simplestepscounter/MainActivity.java
@@ -1,71 +1,51 @@
 package com.example.simplestepscounter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.hardware.*;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity implements SensorEventListener
-{
-    SensorManager sensorManager;
-    TextView tv_Steps;
-
-    boolean isRunning = false;
-
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        count = findViewById(R.id.count);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        tv_Steps = (TextView) findViewById(R.id.textView_Steps);
     }
-
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
-        isRunning = true;
-        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if (countSensor != null)
-        {
-            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-        }
-        else
-        {
-            Toast.makeText(this, "STEP_COUNTER sensor not found", Toast.LENGTH_SHORT).show();
+        activityRunning = true;
+        Sensor countSensor = sensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor,
+                    SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!",
+                    Toast.LENGTH_LONG).show();
         }
     }
-
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
-        isRunning = false;
-//    sensorManager.unregisterListener(this); // this will stop detecting steps!
+        activityRunning = false;
+        // if you unregister the last listener, the hardware will stop detecting
+        // step events
+        // sensorManager.unregisterListener(this);
     }
-
     @Override
-    public void onSensorChanged(SensorEvent event)
-    {
-        if (isRunning)
-        {
-            tv_Steps.setText(String.valueOf(event.values[0]));
+    public void onSensorChanged(SensorEvent event) {
+        if (activityRunning) {
+            count.setText(String.valueOf(event.values[0]));
         }
     }
-
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy)
-    {
-        // Needed this method to skip the error
-        // error: MainActivity is not abstract and does not override abstract method onAccuracyChanged(Sensor,int) in SensorEventListener
-        // public class MainActivity extends AppCompatActivity implements SensorEventListener
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
